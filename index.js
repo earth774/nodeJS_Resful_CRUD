c2fbc19e1175d6f7d84e6c7b70d8bf34053086bc
earth774 <sutthiphongnuanma@gmail.com>
2022-10-16 21:14:19 +0700

    update api for test

--- index.js
+++ index.js
@@ -16,42 +16,66 @@ app.use(function(req, res, next) {
 });
 
 var con = mysql.createConnection({
-            host:"localhost",
-            user:"root",
-            password:"",
-            database:"banking"
+            host:"h1.host.filess.io",
+            user:"banking_speakgain",
+            password:"1e209559216223680a1cf0f6dc87b1a5ecaaca60",
+            database:"banking_speakgain"
         });
 
-	app.get('/data',function(req,res){
-		var sql = "SELECT * FROM `account` WHERE 1";
+	app.get('/',function(req,res){
+		var sql = "SELECT * FROM `customer` WHERE 1";
 		con.query(sql,function(err,row){
 			if(err) throw err;
-			res.json(row);
+			res.json({
+				"status": "success",
+				"data": row
+			});
 		});
 
 	});
 
-	app.post('/insert',function(req,res){
-		var sql ="INSERT INTO `account`(`branch_name`, `balance`) VALUES (?,?)";
-		con.query(sql,[req.body.branch_name,req.body.balance],function(err){
+	app.get('/:id',function(req,res){
+		var sql = "SELECT * FROM `customer` WHERE `id`="+req.params.id;
+		con.query(sql,function(err,row){
+			if(err) throw err;
+			res.json({
+				"status": "success",
+				"data": row[0]
+			});
+		});
+
+	});
+
+	app.post('/',function(req,res){
+		var sql ="INSERT INTO `customer`(`fullname`, `phone_number`) VALUES (?,?)";
+		con.query(sql,[req.body.fullname,req.body.phone_number],function(err){
 			if (err) throw err;
-			res.json({'data':'success'});
+			res.json({
+				"status": "success",
+				"data": "Add data success"
+			});
 		})
 	});
 
-	app.put('/update',function(req,res){
-		var sql ="UPDATE `account` SET `branch_name`=?,`balance`=? WHERE `account_number`=?";
-		con.query(sql,[req.body.branch_name,req.body.balance,req.body.account_number],function(err){
+	app.put('/:id',function(req,res){
+		var sql ="UPDATE `customer` SET `fullname`=?,`phone_number`=? WHERE `id`="+req.params.id;
+		con.query(sql,[req.body.fullname,req.body.phone_number],function(err){
 			if (err) throw err;
-			res.json({'data':'success'});
+			res.json({
+				"status": "success",
+				"data": "Update data success"
+			});
 		})
 	});
 
-	app.delete('/delete/:id',function(req,res){
-		var sql ="DELETE FROM `account` WHERE `account_number`="+req.params.id;
+	app.delete('/:id',function(req,res){
+		var sql ="DELETE FROM `customer` WHERE `id`="+req.params.id;
 		con.query(sql,function(err){
 			if (err) throw err;
-			res.json({'data':'success'});
+			res.json({
+				"status": "success",
+				"data": "Delete data success"
+			});
 		})
 	});
 app.listen(port);
